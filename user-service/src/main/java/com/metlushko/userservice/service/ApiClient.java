3b6bcbc69035b332e6f5ab7b661a25937027dff9
“Artem <3947710@gmail.com>
2024-03-01 14:13:08 +0300

    add Eureka

--- user-service/src/main/java/com/metlushko/userservice/service/ApiClient.java
+++ user-service/src/main/java/com/metlushko/userservice/service/ApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "book-service", url = "http://localhost:8080")
+@FeignClient(value = "book-service")
 public interface ApiClient {
 
     @GetMapping(value = "/api/v1/books/{id}")
