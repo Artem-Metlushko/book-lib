3dfd49353602fb08e079379ae9542e5241f8d9df
“Artem <3947710@gmail.com>
2024-03-11 11:17:08 +0300

    add Security to Gateway

--- gateway/src/main/java/com/metlushko/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/metlushko/gateway/config/GatewayConfig.java
@@ -1,10 +1,22 @@
 package com.metlushko.gateway.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RequiredArgsConstructor
-public class GatewayConfig {
-
-}
+//@Configuration
+//@RequiredArgsConstructor
+//public class GatewayConfig {
+//    public final AuthFilter authFilter;
+//
+////    @Bean
+////    public RouteLocator routes(RouteLocatorBuilder builder) {
+////        return builder.routes()
+////                .route("user-service", r -> r.path("/api/v1/users/**")
+////                        .filters(f -> f.filter(authFilter))
+////                        .uri("lb://user-service"))
+////                .route("book-service", r -> r.path("/api/v1/books/**")
+////                        .filters(f -> f.filter(authFilter))
+////                        .uri("lb://book-service"))
+////                .route("auth-service", r -> r.path("/api/auth/**","/admin","/user")
+////                        .filters(f -> f.filter(authFilter))
+////                        .uri("lb://auth-service"))
+////                .build();
+////    }
+//}
