f4dd0d1f773107fd77306e2eb1f607a673b81698
“Artem <3947710@gmail.com>
2024-04-02 17:18:22 +0300

    mongo deployment

--- book-service/script.sh
+++ book-service/script.sh
@@ -1,7 +1,10 @@
 ./gradlew clean build
-docker build . -t amtlh/book-service:1.0.0
-docker push amtlh/book-service:1.0.0
-
+docker rmi amtlh/book-service:1.0.0 --force
+docker image build . -t amtlh/book-service:1.0.0
+#docker push amtlh/book-service:1.0.0
+minikube image load amtlh/book-service:1.0.0
+#minikube image load postgres
+#minikube image load mongo
 
 
 
