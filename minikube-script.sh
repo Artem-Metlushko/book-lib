68b808881cb4e04e23faf6fc9f052a3d9b719ab6
“Artem <3947710@gmail.com>
2024-04-03 22:01:40 +0300

    refactoring minikube-script.sh

---
+++ minikube-script.sh
@@ -0,0 +1,45 @@
+kubectl delete configmap configmap
+kubectl apply -f ./k8c/configmap.yml
+
+kubectl delete secret postgres-credentials
+kubectl apply -f ./k8c/postgres-credentials.yml
+
+#book-service
+  #postgers-book
+kubectl delete deploy postgres-book
+kubectl apply -f ./k8c/postgres-book-db-deployment.yml
+  #mongo
+kubectl delete deployment.apps/mongo-img
+kubectl apply -f ./k8c/mongo-img-deployment.yml
+  #app
+./gradlew :book-service:clean
+./gradlew :book-service:bootJar
+docker rmi amtlh/book-service:1.0.0 --force
+docker image build ./book-service -t amtlh/book-service:1.0.0
+
+kubectl delete deploy book-service
+kubectl delete svc book-service
+
+minikube image rm amtlh/book-service:1.0.0 --force
+minikube image load amtlh/book-service:1.0.0
+kubectl apply -f ./k8c/book-service-deployment.yml
+
+kubectl get all
+minikube dashboard
+
+#docker push amtlh/book-service:1.0.0
+
+
+
+#./gradlew :user-service:clean
+#./gradlew :user-service:bootJar
+#docker rmi amtlh/user-service:1.0.0 --force
+#docker image build ./user-service -t amtlh/user-service:1.0.0
+#kubectl delete deploy user-service
+#kubectl delete svc user-service
+#minikube image load amtlh/user-service:1.0.0
+
+
+
+#minikube image load postgres
+#minikube image load mongo
